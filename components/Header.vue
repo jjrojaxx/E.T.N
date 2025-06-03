b40312aad190161a52f09120636b42fee68aa034
Joseph leal <jjrojasleal@hotmail.com>
2025-06-02 23:07:38 -0400

    beta 1

--- components/Header.vue
+++ components/Header.vue
@@ -1,18 +1,41 @@
 <script setup></script>
 <template>
-    <header>
-
-
-        <nav class="bg-white border-gray-200 dark:bg-gray-900">
+    <header class="fixed top-0 left-0 right-0 z-50">
+        <nav class="bg-white border-gray-200 dark:bg-gray-950">
             <div class="max-w-screen-xl flex flex-wrap items-center justify-between mx-auto p-4">
-                <a href="https://flowbite.com/" class="flex items-center space-x-3 rtl:space-x-reverse">
-                    <img src="https://flowbite.com/docs/images/logo.svg" class="h-8" alt="Flowbite Logo" />
-                    <span class="self-center text-2xl font-semibold whitespace-nowrap dark:text-white">Flowbite</span>
-                </a>
+                <NuxtLink to="/" class="flex items-center space-x-3 rtl:space-x-reverse">
+                    <img src="../assets/img/logo.png" class="h-15" alt="Flowbite Logo" />
+                </NuxtLink>
                 <div class="flex md:order-2 space-x-3 md:space-x-0 rtl:space-x-reverse">
-                    <button type="button"
-                        class="text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm px-4 py-2 text-center dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800">Get
-                        started</button>
+                    <div class="flex justify-center items-center gap-3">
+                        <a href="#">
+                            <svg class="size-7 hover:text-[#250e8f] transition-all text-gray-800 dark:text-white" aria-hidden="true"
+                                xmlns="http://www.w3.org/2000/svg" fill="currentColor"
+                                viewBox="0 0 24 24">
+                                <path fill-rule="evenodd"
+                                    d="M13.135 6H15V3h-1.865a4.147 4.147 0 0 0-4.142 4.142V9H7v3h2v9.938h3V12h2.021l.592-3H12V6.591A.6.6 0 0 1 12.592 6h.543Z"
+                                    clip-rule="evenodd" />
+                            </svg>
+                        </a>
+                        <a href="#">
+                            <svg class="size-7 hover:text-[#250e8f] transition-all text-gray-800 dark:text-white mt-0.4" aria-hidden="true"
+                                xmlns="http://www.w3.org/2000/svg" fill="none"
+                                viewBox="0 0 24 24">
+                                <path fill="currentColor" fill-rule="evenodd"
+                                    d="M3 8a5 5 0 0 1 5-5h8a5 5 0 0 1 5 5v8a5 5 0 0 1-5 5H8a5 5 0 0 1-5-5V8Zm5-3a3 3 0 0 0-3 3v8a3 3 0 0 0 3 3h8a3 3 0 0 0 3-3V8a3 3 0 0 0-3-3H8Zm7.597 2.214a1 1 0 0 1 1-1h.01a1 1 0 1 1 0 2h-.01a1 1 0 0 1-1-1ZM12 9a3 3 0 1 0 0 6 3 3 0 0 0 0-6Zm-5 3a5 5 0 1 1 10 0 5 5 0 0 1-10 0Z"
+                                    clip-rule="evenodd" />
+                            </svg>
+                        </a>
+                        <a href="#">
+                            <svg class="size-5.5 hover:text-[#250e8f] transition-all text-gray-800 dark:text-white" aria-hidden="true"
+                                xmlns="http://www.w3.org/2000/svg" fill="none"
+                                viewBox="0 0 449.45 515.38">
+                                <path fill="currentColor" fill-rule="evenodd"
+                                    d="M382.31 103.3c-27.76-18.1-47.79-47.07-54.04-80.82-1.35-7.29-2.1-14.8-2.1-22.48h-88.6l-.15 355.09c-1.48 39.77-34.21 71.68-74.33 71.68-12.47 0-24.21-3.11-34.55-8.56-23.71-12.47-39.94-37.32-39.94-65.91 0-41.07 33.42-74.49 74.48-74.49 7.67 0 15.02 1.27 21.97 3.44V190.8c-7.2-.99-14.51-1.59-21.97-1.59C73.16 189.21 0 262.36 0 352.3c0 55.17 27.56 104 69.63 133.52 26.48 18.61 58.71 29.56 93.46 29.56 89.93 0 163.08-73.16 163.08-163.08V172.23c34.75 24.94 77.33 39.64 123.28 39.64v-88.61c-24.75 0-47.8-7.35-67.14-19.96z" />
+                            </svg>
+                        </a>
+
+                    </div>
                     <button data-collapse-toggle="navbar-cta" type="button"
                         class="inline-flex items-center p-2 w-10 h-10 justify-center text-sm text-gray-500 rounded-lg md:hidden hover:bg-gray-100 focus:outline-none focus:ring-2 focus:ring-gray-200 dark:text-gray-400 dark:hover:bg-gray-700 dark:focus:ring-gray-600"
                         aria-controls="navbar-cta" aria-expanded="false">
@@ -26,23 +49,24 @@
                 </div>
                 <div class="items-center justify-between hidden w-full md:flex md:w-auto md:order-1" id="navbar-cta">
                     <ul
-                        class="flex flex-col font-medium p-4 md:p-0 mt-4 border border-gray-100 rounded-lg bg-gray-50 md:space-x-8 rtl:space-x-reverse md:flex-row md:mt-0 md:border-0 md:bg-white dark:bg-gray-800 md:dark:bg-gray-900 dark:border-gray-700">
+                        class="flex flex-col font-medium p-4 md:p-0 mt-4 border border-gray-100 rounded-lg bg-gray-50 md:space-x-8 rtl:space-x-reverse md:flex-row md:mt-0 md:border-0 md:bg-white dark:bg-gray-950 md:dark:bg-gray-950 dark:border-gray-700">
                         <li>
                             <a href="#"
-                                class="block py-2 px-3 md:p-0 text-white bg-blue-700 rounded-sm md:bg-transparent md:text-blue-700 md:dark:text-blue-500"
-                                aria-current="page">Home</a>
+                                class="block py-2 px-3 md:p-0 text-white bg-blue-700 rounded-sm md:bg-transparent md:text-[#150484] transition-all"
+                                aria-current="page">Inicio</a>
                         </li>
                         <li>
                             <a href="#"
-                                class="block py-2 px-3 md:p-0 text-gray-900 rounded-sm hover:bg-gray-100 md:hover:bg-transparent md:hover:text-blue-700 md:dark:hover:text-blue-500 dark:text-white dark:hover:bg-gray-700 dark:hover:text-white md:dark:hover:bg-transparent dark:border-gray-700">About</a>
+                                class="block py-2 px-3 md:p-0 text-gray-900 rounded-sm hover:bg-gray-100 md:hover:bg-transparent md:hover:text-[#250e8f] md:dark:hover:text-[#150484] dark:text-white dark:hover:bg-gray-700 dark:hover:text-white md:dark:hover:bg-transparent dark:border-gray-700 transition-all">Sobre
+                                nosotros</a>
                         </li>
                         <li>
                             <a href="#"
-                                class="block py-2 px-3 md:p-0 text-gray-900 rounded-sm hover:bg-gray-100 md:hover:bg-transparent md:hover:text-blue-700 md:dark:hover:text-blue-500 dark:text-white dark:hover:bg-gray-700 dark:hover:text-white md:dark:hover:bg-transparent dark:border-gray-700">Services</a>
+                                class="block py-2 px-3 md:p-0 text-gray-900 rounded-sm hover:bg-gray-100 md:hover:bg-transparent md:hover:text-[#150484] md:dark:hover:text-[#150484] dark:text-white dark:hover:bg-gray-700 dark:hover:text-white md:dark:hover:bg-transparent dark:border-gray-700 transition-all">Evaluacion</a>
                         </li>
                         <li>
                             <a href="#"
-                                class="block py-2 px-3 md:p-0 text-gray-900 rounded-sm hover:bg-gray-100 md:hover:bg-transparent md:hover:text-blue-700 md:dark:hover:text-blue-500 dark:text-white dark:hover:bg-gray-700 dark:hover:text-white md:dark:hover:bg-transparent dark:border-gray-700">Contact</a>
+                                class="block py-2 px-3 md:p-0 text-gray-900 rounded-sm hover:bg-gray-100 md:hover:bg-transparent md:hover:text-[#150484] md:dark:hover:text-[#150484] dark:text-white dark:hover:bg-gray-700 dark:hover:text-white md:dark:hover:bg-transparent dark:border-gray-700 transition-all">Contacto</a>
                         </li>
                     </ul>
                 </div>
--- pages/index.vue
+++ pages/index.vue
@@ -2,20 +2,19 @@
 </script>
 <template>
     <!-- Hero de pagina -->
-    <section class="bg-white dark:bg-gray-950">
-        <div class="max-w-screen-xl px-4 py-12 mx-auto">
+    <section class="bg-white dark:bg-gray-950 mt-23">
+        <div class="max-w-screen px-4 py-42 mx-auto bg-img">
             <div class="flex flex-col items-center">
                 <h1 class="mb-4 text-3xl font-bold text-gray-900 dark:text-white md:text-5xl lg:text-6xl"><span
-                        class="text-transparent bg-clip-text bg-gradient-to-r to-emerald-600 from-sky-400">Educación con
+                        class="text-[#150484]">Educación con
                         valores</span> y excelencia</h1>
-                <p
-                    class="text-center max-w-2xl mb-6 font-light text-gray-500 lg:mb-8 md:text-lg lg:text-xl dark:text-gray-400">
+                <p class="text-center max-w-2xl mb-6 font-normal text-white lg:mb-8 md:text-lg lg:text-xl">
                     Formamos personas desde el nivel inicial hasta el secundario con un enfoque integral, moderno y
                     humano.
                 </p>
                 <div class="flex flex-row">
                     <a href="#formulario"
-                        class="inline-flex items-center justify-center px-5 py-3 mr-3 text-base font-medium text-white bg-blue-700 rounded-lg hover:bg-blue-800 focus:ring-4 focus:ring-blue-300 dark:focus:ring-blue-900">
+                        class="inline-flex items-center justify-center px-5 py-3 mr-3 text-base font-medium text-white bg-[#150484] rounded-lg hover:bg-[#041184]  focus:ring-4">
                         Solicitar Información
                         <svg class="w-5 h-5 ml-2 -mr-1" fill="currentColor" viewBox="0 0 20 20">
                             <path fill-rule="evenodd"
@@ -24,7 +23,7 @@
                         </svg>
                     </a>
                     <a href="#nosotros"
-                        class="inline-flex items-center justify-center px-5 py-3 text-base font-medium text-gray-900 border border-gray-300 rounded-lg hover:bg-gray-100 focus:ring-4 focus:ring-gray-100 dark:text-white dark:border-gray-700 dark:hover:bg-gray-700 dark:focus:ring-gray-800">
+                        class="inline-flex items-center justify-center px-5 py-3 text-base font-medium text-white border border-white rounded-lg hover:bg-gray-100 focus:ring-4 focus:ring-gray-100 dark:text-white dark:border-white dark:hover:bg-[#041184] dark:hover:border-[#041184] dark:focus:ring-gray-800">
                         Conocé más
                     </a>
                 </div>
@@ -34,12 +33,215 @@
 
     <!-- Seccion Carrreras escolares -->
 
-    <section class="bg-white ">
-        <div class="max-w-screen-xl px-4 py-12 mx-auto flex flex-col items-center">
-            <h2 class="mb-4 text-3xl font-bold text-gray-900 dark:text-gray-950 md:text-5xl lg:text-5xl"><span
-                    class="text-transparent bg-clip-text bg-gradient-to-r to-emerald-600 from-sky-400">Better
-                    Data</span> Scalable AI</h2>
+    <section class="bg-white">
+        <div class="py-8 px-4 mx-auto max-w-screen-xl sm:py-16 lg:px-6">
+            <div class="mb-8 lg:mb-16">
+                <h2 class="mb-4 text-4xl tracking-tight font-bold text-center text-gray-900">Designed for
+                    business teams like yours</h2>
+                <p class="text-gray-500 sm:text-xl dark:text-gray-600 text-center">Here at Flowbite we focus on markets
+                    where
+                    technology, innovation, and capital can unlock <br /> long-term value and drive economic growth.</p>
+            </div>
+            <div class="space-y-8 md:grid md:grid-cols-2 lg:grid-cols-3 md:gap-12 md:space-y-0 justify-center">
+                <div class="flex flex-col items-center">
+                    <div
+                        class="flex justify-center items-center mb-4 w-10 h-10 rounded-full bg-primary-100 lg:h-12 lg:w-12 dark:bg-primary-900 bg-[#150484]">
+                        <svg class="w-5 h-5 lg:w-6 lg:h-6 text-white" aria-hidden="true"
+                            xmlns="http://www.w3.org/2000/svg" width="24" height="24" fill="none" viewBox="0 0 24 24">
+                            <path stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2"
+                                d="M8 17.345a4.76 4.76 0 0 0 2.558 1.618c2.274.589 4.512-.446 4.999-2.31.487-1.866-1.273-3.9-3.546-4.49-2.273-.59-4.034-2.623-3.547-4.488.486-1.865 2.724-2.899 4.998-2.31.982.236 1.87.793 2.538 1.592m-3.879 12.171V21m0-18v2.2" />
+                        </svg>
+                    </div>
+                    <h3 class="mb-2 text-xl font-bold text-gray-900">Administracion</h3>
+                    <p class="text-gray-500 dark:text-gray-700 text-center">Plan it, create it, launch it. Collaborate
+                        seamlessly
+                        with all the organization and hit your marketing goals every month with our marketing plan.</p>
+                </div>
+                <div class="flex flex-col items-center">
+                    <div
+                        class="flex justify-center items-center mb-4 w-10 h-10 rounded-full lg:h-12 lg:w-12 bg-[#150484]">
+                        <svg class="w-5 h-5 text-white lg:w-6 lg:h-6" aria-hidden="true"
+                            xmlns="http://www.w3.org/2000/svg" width="24" height="24" fill="currentColor"
+                            viewBox="0 0 24 24">
+                            <path fill-rule="evenodd"
+                                d="M6 2a2 2 0 0 0-2 2v15a3 3 0 0 0 3 3h12a1 1 0 1 0 0-2h-2v-2h2a1 1 0 0 0 1-1V4a2 2 0 0 0-2-2h-8v16h5v2H7a1 1 0 1 1 0-2h1V2H6Z"
+                                clip-rule="evenodd" />
+                        </svg>
+                    </div>
+                    <h3 class="mb-2 text-xl font-bold text-gray-900">Aduana</h3>
+                    <p class="text-gray-500 dark:text-gray-700 text-center">Protect your organization, devices and stay
+                        compliant
+                        with our structured workflows and custom permissions made for you.</p>
+                </div>
+                <div class="flex flex-col items-center">
+                    <div
+                        class="flex justify-center items-center mb-4 w-10 h-10 rounded-full lg:h-12 lg:w-12 bg-[#150484]">
+                        <svg class="w-5 h-5 text-white lg:w-6 lg:h-6" fill="currentColor" viewBox="0 0 20 20"
+                            xmlns="http://www.w3.org/2000/svg">
+                            <path fill-rule="evenodd"
+                                d="M6 6V5a3 3 0 013-3h2a3 3 0 013 3v1h2a2 2 0 012 2v3.57A22.952 22.952 0 0110 13a22.95 22.95 0 01-8-1.43V8a2 2 0 012-2h2zm2-1a1 1 0 011-1h2a1 1 0 011 1v1H8V5zm1 5a1 1 0 011-1h.01a1 1 0 110 2H10a1 1 0 01-1-1z"
+                                clip-rule="evenodd"></path>
+                            <path
+                                d="M2 13.692V16a2 2 0 002 2h12a2 2 0 002-2v-2.308A24.974 24.974 0 0110 15c-2.796 0-5.487-.46-8-1.308z">
+                            </path>
+                        </svg>
+                    </div>
+                    <h3 class="mb-2 text-xl font-bold text-gray-900">Contabilidad</h3>
+                    <p class="text-gray-500 dark:text-gray-700 text-center">Auto-assign tasks, send Slack messages, and
+                        much more.
+                        Now power up with hundreds of new templates to help you get started.</p>
+                </div>
+                <div class="flex flex-col items-center">
+                    <div
+                        class="flex justify-center items-center mb-4 w-10 h-10 rounded-full bg-primary-100 lg:h-12 lg:w-12 bg-[#150484]">
+                        <svg class="w-5 h-5 text-white lg:w-6 lg:h-6 " fill="currentColor" viewBox="0 0 20 20"
+                            xmlns="http://www.w3.org/2000/svg">
+                            <path fill-rule="evenodd"
+                                d="M11.49 3.17c-.38-1.56-2.6-1.56-2.98 0a1.532 1.532 0 01-2.286.948c-1.372-.836-2.942.734-2.106 2.106.54.886.061 2.042-.947 2.287-1.561.379-1.561 2.6 0 2.978a1.532 1.532 0 01.947 2.287c-.836 1.372.734 2.942 2.106 2.106a1.532 1.532 0 012.287.947c.379 1.561 2.6 1.561 2.978 0a1.533 1.533 0 012.287-.947c1.372.836 2.942-.734 2.106-2.106a1.533 1.533 0 01.947-2.287c1.561-.379 1.561-2.6 0-2.978a1.532 1.532 0 01-.947-2.287c.836-1.372-.734-2.942-2.106-2.106a1.532 1.532 0 01-2.287-.947zM10 13a3 3 0 100-6 3 3 0 000 6z"
+                                clip-rule="evenodd"></path>
+                        </svg>
+                    </div>
+                    <h3 class="mb-2 text-xl font-bold text-gray-900">Electricidad</h3>
+                    <p class="text-gray-500 dark:text-gray-700 text-center">Audit-proof software built for critical
+                        financial
+                        operations like month-end close and quarterly budgeting.</p>
+                </div>
+                <div class="flex flex-col items-center">
+                    <div
+                        class="flex justify-center items-center mb-4 w-10 h-10 rounded-full lg:h-12 lg:w-12 bg-[#150484]">
+                        <svg class="w-5 h-5 lg:w-6 lg:h-6 text-white" fill="currentColor" viewBox="0 0 20 20"
+                            xmlns="http://www.w3.org/2000/svg">
+                            <path
+                                d="M7 3a1 1 0 000 2h6a1 1 0 100-2H7zM4 7a1 1 0 011-1h10a1 1 0 110 2H5a1 1 0 01-1-1zM2 11a2 2 0 012-2h12a2 2 0 012 2v4a2 2 0 01-2 2H4a2 2 0 01-2-2v-4z">
+                            </path>
+                        </svg>
+                    </div>
+                    <h3 class="mb-2 text-xl font-bold text-gray-900">Economia Digital</h3>
+                    <p class="text-gray-500 dark:text-gray-700 text-center">Craft beautiful, delightful experiences for
+                        both
+                        marketing and product with real cross-company collaboration.</p>
+                </div>
+                <div class="flex flex-col items-center">
+                    <div
+                        class="text-center flex justify-center items-center mb-4 w-10 h-10 rounded-full bg-[#150484] lg:h-12 lg:w-12">
+
+                        <svg class="w-5 h-5 text-white lg:w-6 lg:h-6" aria-hidden="true"
+                            xmlns="http://www.w3.org/2000/svg" width="24" height="24" fill="currentColor"
+                            viewBox="0 0 24 24">
+                            <path fill-rule="evenodd"
+                                d="M12 2a1 1 0 0 1 .932.638l7 18a1 1 0 0 1-1.326 1.281L13 19.517V13a1 1 0 1 0-2 0v6.517l-5.606 2.402a1 1 0 0 1-1.326-1.281l7-18A1 1 0 0 1 12 2Z"
+                                clip-rule="evenodd" />
+                        </svg>
+
+                    </div>
+                    <h3 class="mb-2 text-xl font-bold text-gray-900">Turismo</h3>
+                    <p class="text-gray-500 dark:text-gray-700 text-center">Keep your company’s lights on with
+                        customizable,
+                        iterative, and structured workflows built for all efficient teams and individual.</p>
+                </div>
+                <div class="flex flex-col items-center">
+                    <div
+                        class="text-center flex justify-center items-center mb-4 w-10 h-10 rounded-full bg-[#150484] lg:h-12 lg:w-12">
+                        <svg class="w-5 h-5 text-white lg:w-6 lg:h-6 " fill="currentColor" viewBox="0 0 20 20"
+                            xmlns="http://www.w3.org/2000/svg">
+                            <path fill-rule="evenodd"
+                                d="M11.49 3.17c-.38-1.56-2.6-1.56-2.98 0a1.532 1.532 0 01-2.286.948c-1.372-.836-2.942.734-2.106 2.106.54.886.061 2.042-.947 2.287-1.561.379-1.561 2.6 0 2.978a1.532 1.532 0 01.947 2.287c-.836 1.372.734 2.942 2.106 2.106a1.532 1.532 0 012.287.947c.379 1.561 2.6 1.561 2.978 0a1.533 1.533 0 012.287-.947c1.372.836 2.942-.734 2.106-2.106a1.533 1.533 0 01.947-2.287c1.561-.379 1.561-2.6 0-2.978a1.532 1.532 0 01-.947-2.287c.836-1.372-.734-2.942-2.106-2.106a1.532 1.532 0 01-2.287-.947zM10 13a3 3 0 100-6 3 3 0 000 6z"
+                                clip-rule="evenodd"></path>
+                        </svg>
+                    </div>
+                    <h3 class="mb-2 text-xl font-bold text-gray-900">Telematica</h3>
+                    <p class="text-gray-500 dark:text-gray-700 text-center">Keep your company’s lights on with
+                        customizable,
+                        iterative, and structured workflows built for all efficient teams and individual.</p>
+                </div>
+            </div>
+        </div>
+    </section>
+
+    <section class="bg-white dark:bg-gray-950">
+        <div class="gap-16 items-center py-8 px-4 mx-auto max-w-screen-xl lg:grid lg:grid-cols-2 lg:py-16 lg:px-6">
+            <div class="font-light text-gray-500 sm:text-lg dark:text-gray-400">
+                <h2 class="mb-4  text-4xl tracking-tight font-bold text-gray-900 dark:text-white">We didn't reinvent
+                    the wheel</h2>
+                <p class="mb-4">We are strategists, designers and developers. Innovators and problem solvers. Small
+                    enough to be simple and quick, but big enough to deliver the scope you want at the pace you need.
+                    Small enough to be simple and quick, but big enough to deliver the scope you want at the pace you
+                    need.</p>
+                <p>We are strategists, designers and developers. Innovators and problem solvers. Small enough to be
+                    simple and quick.</p>
+            </div>
+            <div class="grid grid-cols-2 gap-4 mt-8">
+                <img class="w-full rounded-lg"
+                    src="https://flowbite.s3.amazonaws.com/blocks/marketing-ui/content/office-long-2.png"
+                    alt="office content 1">
+                <img class="mt-4 w-full lg:mt-10 rounded-lg"
+                    src="https://flowbite.s3.amazonaws.com/blocks/marketing-ui/content/office-long-1.png"
+                    alt="office content 2">
+            </div>
+        </div>
+    </section>
+
+    <section class="bg-white">
+        <div class="gap-4 items-center py-8 px-4 mx-auto max-w-screen-xl lg:grid lg:grid-cols-1 lg:py-16 lg:px-6">
+            <div class="font-light text-gray-500 sm:text-lg dark:text-gray-400">
+                <h2 class="mb-4 text-center text-4xl tracking-tight font-bold text-gray-900">We didn't
+                    reinvent
+                    the wheel</h2>
+                <p class="text-center mb-6 text-gray-700 font-normal">We are strategists, designers and developers. Innovators and problem
+                    solvers. Small
+                    enough to be simple and quick, but big enough to deliver the scope you want at the pace you need.
+                    Small enough to be simple and quick, but big enough to deliver the scope you want at the pace you
+                    need.</p>
+            </div>
+            <div class="grid grid-cols-4 gap-4">
+                <img class="w-full rounded-lg"
+                    src="https://flowbite.s3.amazonaws.com/blocks/marketing-ui/content/office-long-2.png"
+                    alt="office content 1">
+                <img class="w-full rounded-lg"
+                    src="https://flowbite.s3.amazonaws.com/blocks/marketing-ui/content/office-long-1.png"
+                    alt="office content 2">
+                <img class="w-full rounded-lg"
+                    src="https://flowbite.s3.amazonaws.com/blocks/marketing-ui/content/office-long-1.png"
+                    alt="office content 2">
+                <img class="w-full rounded-lg"
+                    src="https://flowbite.s3.amazonaws.com/blocks/marketing-ui/content/office-long-1.png"
+                    alt="office content 2">
+            </div>
+            <div class="grid grid-cols-3 gap-4">
+                <img class="w-full rounded-lg"
+                    src="https://flowbite.s3.amazonaws.com/blocks/marketing-ui/content/office-long-2.png"
+                    alt="office content 1">
+                <img class="w-full rounded-lg"
+                    src="https://flowbite.s3.amazonaws.com/blocks/marketing-ui/content/office-long-1.png"
+                    alt="office content 2">
+                <img class="w-full rounded-lg"
+                    src="https://flowbite.s3.amazonaws.com/blocks/marketing-ui/content/office-long-1.png"
+                    alt="office content 2">
+            </div>
         </div>
     </section>
 </template>
-<style scoped></style>
+<style scoped>
+.bg-img {
+    background-image: url('../assets/img/bg-hero.jpg');
+    background-size: cover;
+    background-position: center;
+}
+
+.bg-img::before {
+    content: "";
+    position: absolute;
+    top: 0;
+    left: 0;
+    right: 0;
+    bottom: 0;
+    background-color: rgba(0, 0, 0, 0.897);
+    /* negro con opacidad */
+    z-index: 1;
+}
+
+.bg-img>* {
+    position: relative;
+    z-index: 2;
+}
+</style>
