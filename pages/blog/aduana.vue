7afeec74ad45c6c95d8347542e4b24d74e342c01
Duntri <rubensalas0908@gmail.com>
2025-06-07 13:34:11 -0400

    nueva pagina aduana

---
+++ pages/blog/aduana.vue
@@ -0,0 +1,6 @@
+<script setup></script>
+<template>
+<h1>jose es gey</h1>
+<p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Qui magnam magni perferendis eos! Voluptatem atque, tempore sint dolores reprehenderit voluptatum, quam deleniti ipsam assumenda nemo quas culpa? Sequi, illum sint.</p>
+</template>
+<style scoped></style>
