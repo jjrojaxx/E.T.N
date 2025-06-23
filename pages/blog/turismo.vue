33dd3ed576be5caa3d591875353b303c816bacc2
Duntri <rubensalas0908@gmail.com>
2025-06-23 06:11:57 -0400

    listo

--- pages/blog/turismo.vue
+++ pages/blog/turismo.vue
@@ -439,7 +439,7 @@ onBeforeUnmount(() => {
         <div
             class="gap-8 items-center py-8 px-4 mx-auto max-w-screen-xl xl:gap-16 md:grid md:grid-cols-2 sm:py-16 lg:px-6">
             <img class="w-full dark:hidden rounded-xl" src="../../assets/img/foto4.jpg" alt="dashboard image">
-            <img class="w-full rounded-xl hidden dark:block" src="../../assets/img/Turismo2.jpg" alt="dashboard image">
+            <img class="w-full rounded-xl hidden dark:block" src="../../assets/img/turismo2.jpg" alt="dashboard image">
             <div class="mt-4 md:mt-0">
                 <h2 class="mb-4 text-4xl tracking-tight font-bold text-gray-900 dark:text-white">Perfil del Estudiante</h2>
                 <p class="mb-6 font-normal text-gray-500 md:text-lg">El estudiante de turismo se caracteriza por poseer una combinación de habilidades sociales, culturales y organizativas que le permiten desenvolverse con éxito en el sector turístico. Este perfil requiere interés por la cultura, capacidad de comunicación, atención al cliente, y habilidades para la gestión de servicios y recursos turísticos en un entorno dinámico y globalizado.</p>
