01f16921e2a14752e0ced255a261603dceb6c686
Duntri <rubensalas0908@gmail.com>
2025-06-23 04:08:54 -0400

    cambio en el footer

--- components/Footer.vue
+++ components/Footer.vue
@@ -12,7 +12,7 @@
             </div>
             <hr class="my-6 border-gray-200 sm:mx-auto dark:border-gray-700 lg:my-8" />
             <span class="block text-center text-sm text-gray-500 sm:text-center dark:text-gray-400">© 2025 <NuxtLink
-                    to="/" class="hover:underline">E.T.C.N Manuel Atanasio Girardot™</NuxtLink>. All Rights Reserved.</span>
+                    to="/" class="hover:underline">E.T.C.N Manuel Atanasio Girardot™</NuxtLink></span>
         </div>
     </footer>
 </template>
